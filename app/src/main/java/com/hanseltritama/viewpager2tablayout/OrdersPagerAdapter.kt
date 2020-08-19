288ab297412aa822f3a4a5ac715ef82ed47c4aa2
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2020-08-18 20:07:16 -0700

    added ViewPager2

---
+++ app/src/main/java/com/hanseltritama/viewpager2tablayout/OrdersPagerAdapter.kt
@@ -0,0 +1,23 @@
+package com.hanseltritama.viewpager2tablayout
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class OrdersPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+
+    override fun getItemCount(): Int {
+        return 0
+    }
+
+    override fun createFragment(position: Int): Fragment {
+
+        return when (position) {
+            0 -> PendingOrdersFragment()
+            1 -> ConfirmedOrdersFragment()
+            else -> DeliveredOrdersFragment()
+        }
+
+    }
+
+}
